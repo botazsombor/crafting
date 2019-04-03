eaea5f69b3efab31decd5eb9f581ca147d996fee
Krityu <orszacska@gmail.com>
2019-04-03 15:25:19 +0200

    Test cases option problem

--- backend/src/test/java/hu/elte/CraftingGame/CraftingGame/CraftingGameApplicationTests.java
+++ backend/src/test/java/hu/elte/CraftingGame/CraftingGame/CraftingGameApplicationTests.java
@@ -1,16 +1,42 @@
 package hu.elte.CraftingGame.CraftingGame;
 
+import java.util.Optional;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import hu.elte.CraftingGame.entities.Element;
+import hu.elte.CraftingGame.repositories.ElementRepository;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class CraftingGameApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private ElementRepository elementRepository;
+	
+    @Test
+    public void whenFindByName_thenReturnElement() {
+        // given
+    	
+        Element element = new Element("child","firstparent","secondparent", null);
+        entityManager.persist(element);
+        entityManager.flush();
+     
+        // when
+        Optional<Element> found = elementRepository.findByElementName(element.getElementName());
+        
+        // then
+        Assert.assertEquals(1, 1);
+    }
 
 }
