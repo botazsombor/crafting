a229e9cfdf5568857274f61cd2a61bdf8a9f18eb
Krityu <orszacska@gmail.com>
2019-04-05 20:50:53 +0200

    4 positive test cases with 1 fail# Please enter the commit message for your changes. Lines starting

--- backend/src/test/java/hu/elte/CraftingGame/CraftingGameApplicationTests.java
+++ backend/src/test/java/hu/elte/CraftingGame/CraftingGameApplicationTests.java
@@ -8,11 +8,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import hu.elte.CraftingGame.entities.Element;
+import hu.elte.CraftingGame.entities.User;
+import hu.elte.CraftingGame.entities.User.Role;
 import hu.elte.CraftingGame.repositories.ElementRepository;
+import hu.elte.CraftingGame.repositories.UserRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -22,6 +24,8 @@ public class CraftingGameApplicationTests {
     private TestEntityManager entityManager;
     @Autowired
     private ElementRepository elementRepository;
+    @Autowired
+    private UserRepository userRepository;
 	
     @Test
     public void whenFindByName_thenReturnElement() {
@@ -35,7 +39,43 @@ public class CraftingGameApplicationTests {
         Optional<Element> found = elementRepository.findByElementName(element.getElementName());
         
         // then
-        Assert.assertEquals(1, 1);
+        Assert.assertEquals(found.get(), element);
     }
+    
+    @Test
+    public void whenFindByFirstParentAndSecondParent_thenReturnElement() {
+    	Element element = new Element("child","firstparent","secondparent", null);
+        entityManager.persist(element);
+        entityManager.flush();
+        
+        Optional<Element> found = elementRepository.findByFirstParentAndSecondParent("firstparent", "secondparent");
+        
+        Assert.assertEquals(found.get(), element);
+    }
+    
+    
+    //still failure
+    @Test
+    public void whenFindBySecondParentAndFirstParent_thenReturnElement() {
+    	Element element = new Element("child","firstparent","secondparent", null);
+        entityManager.persist(element);
+        entityManager.flush();
+        
+        Optional<Element> found = elementRepository.findByFirstParentAndSecondParent("secondparent", "firstparent");
+        
+        Assert.assertEquals(found.get(), element);
+    }
+    
+    @Test
+    public void whenFindByUsername_thenReturnUser() {
+    	User user = new User("username", "password", Role.ROLE_USER, null);
+        entityManager.persist(user);
+        entityManager.flush();
+        
+        Optional<User> found = userRepository.findByUsername("username");
+        
+        Assert.assertEquals(found.get(), user);
+    }
+    
 
 }
